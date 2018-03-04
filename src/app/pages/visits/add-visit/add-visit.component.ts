81e8eb36613cb129460c4f768834d491ae516bb6
Ahmed Abdelfattah <ahmed.m.abdelftah@gmail.com>
2018-03-04 18:36:41 +0200

    add visit

--- src/app/pages/visits/add-visit/add-visit.component.ts
+++ src/app/pages/visits/add-visit/add-visit.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 import { LocalDataSource } from 'ng2-smart-table';
-
+import { Http, RequestOptions, RequestMethod, Headers } from '@angular/http';
 import { SmartTableService } from '../../../@core/data/smart-table.service';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
 
 @Component({
   selector: 'ngx-smart-table',
@@ -14,12 +17,12 @@ import { SmartTableService } from '../../../@core/data/smart-table.service';
 })
 export class AddVisitComponent {
 
-  visit = { hallName:String, time:'', description:''};
+  visit = { hallName: '', time: '', description: '' };
   file: File;
 
 
-  constructor(private service: SmartTableService) {
-    
+  constructor(private service: SmartTableService, private http: Http) {
+
   }
 
   addVisit() {
@@ -27,11 +30,27 @@ export class AddVisitComponent {
   }
 
   onChange(event: EventTarget) {
-    let eventObj: MSInputMethodContext = <MSInputMethodContext> event;
-    let target: HTMLInputElement = <HTMLInputElement> eventObj.target;
+    let eventObj: MSInputMethodContext = <MSInputMethodContext>event;
+    let target: HTMLInputElement = <HTMLInputElement>eventObj.target;
     let files: FileList = target.files;
-    this.file = files[0];
-    console.log(this.file);
+    let fileToUpload = files[0];
+    console.log(fileToUpload);
+    let headers = new Headers();
+    /** No need to include Content-Type in Angular 4 */
+    headers.append('Content-Type', 'multipart/form-data');
+    headers.append('Accept', 'application/json');
+
+    const endpoint = 'http://localhost/gem/';
+    const formData: FormData = new FormData();
+    formData.append('fileKey', fileToUpload, fileToUpload.name);
+    return this.http
+      .get(endpoint, {params: {purpose: 'upload_file',fileKey:fileToUpload,fileName:fileToUpload.name}})
+      .map(() => { return true; })
+      .catch(error => Observable.throw(error))
+      .subscribe(
+        data => console.log('success'),
+        error => console.log(error)
+      );
   }
 
 
